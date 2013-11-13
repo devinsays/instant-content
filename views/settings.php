b1040c2ff8fbcd10644e82b51b9cbaa9f8c1fb34
Devin Price <devin@wptheming.com>
2013-11-13 14:42:55 -0600

    Remove double spaces from strings.

--- views/settings.php
+++ views/settings.php
@@ -59,7 +59,7 @@
 						<p><label class="description" for="instant_content[license]">
 						<?php if ( $status == 'valid' ) {
 							_e( 'Your license is valid.', 'instant-content' );
-						} elseif ( $status == 'error' ) {
+						} elseif ( 'error' === $status ) {
 							_e( 'There was an error validating the license.  Try saving again.', 'instant-content' );
 						} else {
 							printf(
